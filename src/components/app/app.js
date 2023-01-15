ef9d5cee7e44069dfdebead3d68dd97bda238cce
Sarkhan <abdullayevsarxan92@gmail.com>
2023-01-15 15:24:00 +0400

    feat: adding star to name

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,19 +7,26 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
+    const data = [
+        { name: 'Jonh', salary: 800, increase: false, id: 1 },
+        { name: 'Alex', salary: 1800, increase: true, id: 2 },
+        { name: 'Max', salary: 2800, increase: false, id: 3 },
+    ];
+    // key prop нужен что реакт понял что не надо менять весь элемент
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>
+
+            <EmployeesList data={data} />
+            <EmployeesAddForm />
         </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,15 +1,31 @@
-import EmployeesListItem from "../employees-list-item/employees-list-item";
+import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = () => {
-    return (
-        <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-        </ul>
-    )
-}
+const EmployeesList = ({ data }) => {
+    const elements = data.map((item) => {
+        const { id, ...itemProps } = item;
+
+        return <EmployeesListItem key={id} {...itemProps} />;
+    });
+
+    return <ul className="app-list list-group">{elements}</ul>;
+};
 
 export default EmployeesList;
+
+// <EmployeesListItem name="Jonh" salary={800} />
+// const elements = data.map((item) => {
+//     return <EmployeesListItem name={item.name} salary={item.salary} />;
+// });
+
+// if id didn't come from server
+// const EmployeesList = ({ data }) => {
+//     const elements = data.map((item, i) => {
+//         const { id, ...itemProps } = item;
+
+//         return <EmployeesListItem key={i} {...itemProps} />;
+//     });
+
+//     return <ul className="app-list list-group">{elements}</ul>;
+// };
