b115dedf3bcc83540b06a6096e8be8f080faec31
Sarkhan <abdullayevsarxan92@gmail.com>
2023-01-16 23:33:57 +0400

    feat: adding functional with adding and removing employees also counting overall quantity

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,11 +2,24 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDelete, onToggleProp }) => {
     const elements = data.map((item) => {
         const { id, ...itemProps } = item;
 
-        return <EmployeesListItem key={id} {...itemProps} />;
+        return (
+            <EmployeesListItem
+                key={id}
+                {...itemProps}
+                onDelete={() => onDelete(id)} // как props можно давать все, даже функции и так делаем свое событие
+                // 2 onDelete приходит с аргумента который был передан в APP
+                onToggleProp={(e) =>
+                    onToggleProp(
+                        id,
+                        e.currentTarget.getAttribute('data-toggle')
+                    )
+                }
+            />
+        );
     });
 
     return <ul className="app-list list-group">{elements}</ul>;
